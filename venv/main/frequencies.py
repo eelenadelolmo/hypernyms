190edfe9d486e0e8476b0ef7c87eceea325716fe
elenadelolmo <eelena.delolmo@gmail.com>
2021-04-06 02:35:06 +0200

    uploading freeqq script plus topicrank processed data with absolute freqs

---
+++ venv/main/frequencies.py
@@ -0,0 +1,42 @@
+import re
+import os
+
+
+def freq_calc(d, all):
+    with open(all, 'r') as f:
+        text_all = f.read()
+        f.close()
+
+    with open(d, 'r') as f:
+        text = f.read()
+        if re.search(r'Keywords: ?\n.+', text, re.DOTALL):
+            keywords = re.search(r'Keywords: ?\n(.+)', text, re.DOTALL).group(1)
+        else:
+            keywords = text
+        kw_list = keywords.split('\n')
+        kw_freq_list = list()
+        for kw in kw_list:
+            kw_clean = re.sub('^- ?', '', kw)
+            if len(kw_clean) > 0:
+                kw_freq_list.append((kw_clean, text_all.count(kw_clean)))
+        f.close()
+
+    keywords_replace = str()
+    for k, f_k in kw_freq_list:
+        keywords_replace += '- ' + k + ' (' + str(f_k) + ')\n'
+
+    text_replace = re.sub(keywords, keywords_replace, text)
+
+    with open(d, 'w') as f:
+        f.write(text_replace)
+        f.close()
+
+
+
+dir_all = 'corpus/Medical/txt_all.txt'
+"""
+dir_docs = 'corpus/Medical/kw/pke/topic_rank'
+docs = os.listdir(dir_docs)
+for doc in docs:
+    freq_calc(dir_docs + '/' + doc, dir_all)
+"""
