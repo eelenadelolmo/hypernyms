93e4e90603e99f17f2d990a50725b4684e1bf796
elenadelolmo <eelena.delolmo@gmail.com>
2021-04-06 11:21:49 +0200

    freq script ready

--- venv/main/frequencies.py
+++ venv/main/frequencies.py
@@ -2,6 +2,8 @@ import re
 import os
 
 
+# The first argument is the path to a document and the second argument is the patch to a document with the whole corpora
+# Rewrites the document with the absolute frequency of every keyword in the whole corpora
 def freq_calc(d, all):
     with open(all, 'r') as f:
         text_all = f.read()
@@ -23,14 +25,13 @@ def freq_calc(d, all):
 
     keywords_replace = str()
     for k, f_k in kw_freq_list:
-        keywords_replace += '- ' + k + ' (' + str(f_k) + ')\n'
+        keywords_replace += '- ' + re.escape(k) + ' (' + re.escape(str(f_k)) + ')\n'
 
     text_replace = re.sub(keywords, keywords_replace, text)
 
     with open(d, 'w') as f:
         f.write(text_replace)
         f.close()
-
 
 
 dir_all = 'corpus/Medical/txt_all.txt'
@@ -40,3 +41,78 @@ docs = os.listdir(dir_docs)
 for doc in docs:
     freq_calc(dir_docs + '/' + doc, dir_all)
 """
+
+dir_docs = 'corpus/Medical/kw/text_rank'
+docs = os.listdir(dir_docs)
+for doc in docs:
+    freq_calc(dir_docs + '/' + doc, dir_all)
+
+dir_docs = 'corpus/Medical/kw/yake'
+docs = os.listdir(dir_docs)
+for doc in docs:
+    freq_calc(dir_docs + '/' + doc, dir_all)
+
+dir_docs = 'corpus/Medical/keybert/keybert'
+docs = os.listdir(dir_docs)
+for doc in docs:
+    freq_calc(dir_docs + '/' + doc, dir_all)
+
+dir_docs = 'corpus/Medical/keybert/keybert_maxMargRelevance'
+docs = os.listdir(dir_docs)
+for doc in docs:
+    freq_calc(dir_docs + '/' + doc, dir_all)
+
+dir_docs = 'corpus/Medical/keybert/keybert_maxSum'
+docs = os.listdir(dir_docs)
+for doc in docs:
+    freq_calc(dir_docs + '/' + doc, dir_all)
+
+dir_docs = 'corpus/Medical/rake/txt_all_rake_degreeFreqRatio'
+docs = os.listdir(dir_docs)
+for doc in docs:
+    freq_calc(dir_docs + '/' + doc, dir_all)
+
+dir_docs = 'corpus/Medical/rake/txt_all_rake_degreeFreqRatio_length'
+docs = os.listdir(dir_docs)
+for doc in docs:
+    freq_calc(dir_docs + '/' + doc, dir_all)
+
+dir_docs = 'corpus/Medical/rake/txt_all_rake_wordDegree_length'
+docs = os.listdir(dir_docs)
+for doc in docs:
+    freq_calc(dir_docs + '/' + doc, dir_all)
+
+dir_docs = 'corpus/Medical/rake/txt_all_rake_wordFreq_length'
+docs = os.listdir(dir_docs)
+for doc in docs:
+    freq_calc(dir_docs + '/' + doc, dir_all)
+
+dir_docs = 'corpus/Medical/tf_idf/tf_idf'
+docs = os.listdir(dir_docs)
+for doc in docs:
+    freq_calc(dir_docs + '/' + doc, dir_all)
+
+dir_docs = 'corpus/Medical/tf_idf/tf_idf_sklearn'
+docs = os.listdir(dir_docs)
+for doc in docs:
+    freq_calc(dir_docs + '/' + doc, dir_all)
+
+"""
+dir_all_kw = 'corpus/Medical/txt_all_rake_degreeFreqRatio.txt'
+freq_calc(dir_all_kw, dir_all)
+"""
+
+dir_all_kw = 'corpus/Medical/txt_all_rake_degreeFreqRatio_length.txt'
+freq_calc(dir_all_kw, dir_all)
+
+dir_all_kw = 'corpus/Medical/txt_all_rake_wordDegree_length.txt'
+freq_calc(dir_all_kw, dir_all)
+
+dir_all_kw = 'corpus/Medical/txt_all_rake_wordFreq_length.txt'
+freq_calc(dir_all_kw, dir_all)
+
+dir_all_kw = 'corpus/Medical/txt_all_text_rank.txt'
+freq_calc(dir_all_kw, dir_all)
+
+dir_all_kw = 'corpus/Medical/txt_all_yake.txt'
+freq_calc(dir_all_kw, dir_all)
