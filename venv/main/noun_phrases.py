fe59fc4cdc0d3a3d40e830a32f685be136674a77
elenadelolmo <eelena.delolmo@gmail.com>
2021-04-22 18:51:41 +0200

    processing corpora by paragraphs

--- venv/main/noun_phrases.py
+++ venv/main/noun_phrases.py
@@ -32,6 +32,15 @@ def delete_overlapping(np_list):
                 # raise ValueError('partial overlap?')
     return [elem for elem in np_list if elem]
 
+
+
+# Approximately equivalent to '<DET>? <NUM>* (<ADJ> <CONJ>?)* (<NOUN>|<PROPN> (<PART> <CONJ>?)*)+'
+pattern_np = [{"POS": "DET", "OP":"?"}, {"POS": "NUM", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "NOUN", "OP":"+"}, {"POS": "ADV", "OP":"*"}, {"TAG": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"TAG": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"TAG": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADV", "OP":"*"}]
+pattern_pnp = [{"POS": "DET", "OP":"?"}, {"POS": "NUM", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "NOUN", "OP":"*"}, {"POS": "PROPN", "OP":"+"}, {"POS": "NOUN", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"TAG": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"TAG": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"TAG": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}]
+pattern_np_pp = [{"POS": "DET", "OP":"?"}, {"POS": "NUM", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "NOUN", "OP":"+"}, {"POS": "ADV", "OP":"*"}, {"TAG": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"TAG": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"TAG": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADP"}, {"POS": "DET", "OP":"?"}, {"POS": "NUM", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "NOUN", "OP":"+"}, {"POS": "ADV", "OP":"*"}, {"POS": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"TAG": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"TAG": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADV", "OP":"*"}]
+
+
+
 # For testing only
 text = """
 I am only testing the big big big well performing model. 
@@ -41,34 +50,45 @@ I want to match only the first noun phrase of every sentence of the text.
 It was a time well spent.
 """
 
-all = 'corpus/Medical/txt_all.txt'
-
-with open(all) as f:
-    text = f.read().lower()
-
-doc = textacy.make_spacy_doc(text, lang=u'en_core_web_trf')
-
 """
 for token in doc:
   print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_, token.shape_, token.is_alpha, token.is_stop)
 """
 
-# Approximately equivalent to '<DET>? <NUM>* (<ADJ> <CONJ>?)* (<NOUN>|<PROPN> (<PART> <CONJ>?)*)+'
-pattern_np = [{"POS": "DET", "OP":"?"}, {"POS": "NUM", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "NOUN", "OP":"+"}, {"POS": "ADV", "OP":"*"}, {"TAG": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"TAG": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"TAG": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADV", "OP":"*"}]
-pattern_pnp = [{"POS": "DET", "OP":"?"}, {"POS": "NUM", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "NOUN", "OP":"*"}, {"POS": "PROPN", "OP":"+"}, {"POS": "NOUN", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"TAG": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"TAG": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"TAG": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}]
-pattern_np_pp = [{"POS": "DET", "OP":"?"}, {"POS": "NUM", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "NOUN", "OP":"+"}, {"POS": "ADV", "OP":"*"}, {"TAG": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"TAG": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"TAG": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADP"}, {"POS": "DET", "OP":"?"}, {"POS": "NUM", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADJ", "OP":"*"}, {"POS": "NOUN", "OP":"+"}, {"POS": "ADV", "OP":"*"}, {"POS": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"TAG": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"TAG": "VBN", "OP":"*"}, {"POS": "CONJ", "OP":"*"}, {"POS": "ADV", "OP":"*"}, {"POS": "ADV", "OP":"*"}]
 
-salida_np = list(textacy.extract.token_matches(doc, pattern_np))
-salida_np = delete_overlapping(salida_np
-                               )
-salida_pnp = list(textacy.extract.token_matches(doc, pattern_pnp))
+
+all = 'corpus/Medical/txt_all.txt'
+
+with open(all) as f:
+    text = f.read().lower()
+
+paras_list = text.split('\n\n\n')
+
+
+# List of common noun phrases
+salida_np = list()
+
+# List of proper noun phrases
+salida_pnp = list()
+
+# List of noun phrases with prepositional complements
+salida_np_pp = list()
+
+
+for para in paras_list:
+    doc = textacy.make_spacy_doc(para, lang=u'en_core_web_trf')
+    salida_np.extend(list(textacy.extract.token_matches(doc, pattern_np)))
+    salida_pnp.extend(list(textacy.extract.token_matches(doc, pattern_pnp)))
+    salida_np_pp.extend(list(textacy.extract.token_matches(doc, pattern_np_pp)))
+
+
+# Keeping only longest matches and adding proper and common noun phrases together
+salida_np = delete_overlapping(salida_np)
 salida_pnp = delete_overlapping(salida_pnp)
-
 salida_np_pnp = salida_np + salida_pnp
-
-salida_np_pp = list(textacy.extract.token_matches(doc, pattern_np_pp))
 salida_np_pp = delete_overlapping(salida_np_pp)
 
+# Saving results to txt
 with open('corpus/Medical/txt_all_noun_phrases.txt', 'w') as f_w:
     for e in salida_np_pnp:
         f_w.write('-' + e.text + '\n')
